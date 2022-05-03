7c88dce3f6677d492676d05e292afe4aacad7422
pritamdk <pritamdk1994@gmail.com>
2022-05-03 21:26:16 +0530

    Added : Welcome

---
+++ review.sh
@@ -0,0 +1,11 @@
+word=a
+
+if (($word=="b"))
+then
+	echo "the condition matches with b: is true"
+elif(($word=="a"))
+then
+	echo "the condition matches with a: is true"
+else
+	echo "both condition are not matched: is false"
+fi
